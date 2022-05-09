99840a8e7864fca098d86810c244e25f4e9bc2a6
Daniyal-Murtaza <86107799+Daniyal-Murtaza@users.noreply.github.com>
2022-05-09 18:54:08 +0500

    polymorphism

---
+++ Pointers_to_Derived_class.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class BaseClass
+{
+public:
+    int var_base;
+    void display()
+    {
+        cout << "Value of var_base is: " << var_base << endl;
+    }
+};
+
+class DerivedClass : public BaseClass
+{
+public:
+    int var_derived;
+    void display()
+    {
+        cout << "Value of var_base is: " << var_base << endl;
+        cout << "Value of var_derived is: " << var_derived << endl;
+    }
+};
+
+int main()
+{
+    BaseClass BC_obj;
+    DerivedClass DC_obj;
+
+    // If we make a base_class_pointer then we can only access the data members and function members of the base class.
+
+    BaseClass *base_class_pointer = &DC_obj; // pointing base_class_pointer to derived class object
+    base_class_pointer->var_base = 4;
+    cout << "base class pointer" << endl;
+    base_class_pointer->display();
+    cout << endl;
+
+    // base_class_pointer->var_derived = 42; we cannot do this (it will throw an error)
+
+    //***************************************************************************************************************
+
+    // If we make a derived_class_pointer then we can access the data members and function members
+    // of the base class as well as derived class.
+
+    DerivedClass *derived_class_pointer = &DC_obj; // pointing derived_class_pointer to derived class object
+    derived_class_pointer->var_derived = 53;
+    derived_class_pointer->var_base = 78;
+    cout << "Derived class pointer" << endl;
+    derived_class_pointer->display();
+
+    return 0;
+}
--- polymorphism.cpp
+++ polymorphism.cpp
@@ -1,10 +1,15 @@
+/*
+Polymorphism:
+one name having multiple forms
+e.g function overloading, operator overloading, virtual functions
+Polymorphism can be of two types:
 
-// Polymorphism:
-// one name having multiple forms
-// e.g function overloading, operator overloading, virtual functions
-// Polymorphism can be of two types:
-// 1. Compile time polymorphism
-// 1.1- function overloading
-// 1.2- operator overloading
-// 2. Run time polymorphism
-// 2.1- virtual functions
+1. Compile time polymorphism - Early binding
+1.1- function overloading
+1.2- operator overloading
+
+2. Run time polymorphism - late binding
+2.1- virtual functions
+*/
+
+
