c868f0c38673a9a567016c2ad8b60fedd75b33e2
Daniyal-Murtaza <86107799+Daniyal-Murtaza@users.noreply.github.com>
2022-05-10 00:54:21 +0500

    pureVirtual-abstractClass

---
+++ pureVirtual-abstractClass.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// Abstract base class and PURE virtual function are inter related to each other
+// Abstract base class is those which contains ATLEAST ONE PURE VIRTUAL FUNCTION.
+
+
+int main(){
+
+    return 0;
+}
