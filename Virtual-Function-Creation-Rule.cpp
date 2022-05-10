d7ccaadd571d0ffc42087951f1be3f11e757349c
Daniyal-Murtaza <86107799+Daniyal-Murtaza@users.noreply.github.com>
2022-05-10 10:12:33 +0500

    virtual-function-example-and-rules

--- Virtual-Function-Creation-Rule.cpp
+++ Virtual-Function-Creation-Rule.cpp
@@ -56,15 +56,47 @@ public:
 
 int main()
 {
-    CWHVideo vid("django", 45.89, 4.23);
-    vid.display();
+    string title;
+    float rating, vlen;
+    float words;
+
+    // for video
+    title = "django vid";
+    rating = 45.89;
+    vlen = 4.23;
+
+    CWHVideo vid(title, rating, vlen);
+    // vid.display();
+
+    // for text
+    title = "django text";
+    rating = 78.39;
+    words = 4.19;
+
+    CWHText tex(title, rating, words);
+    // tex.display();
+
+    // making base class pointers and pointing it towards derived classes object
+    CWH *tut[2];
+    tut[0] = &vid;
+    tut[1] = &tex;
+
+    // see the result with and without virtual keyword in Line 18
+
+    tut[0]->display();
     cout << endl;
-
-    CWHText tex("pypy", 422.289, 4.19);
-    vid.display();
-
-    // making base class (CWH) pointer:
-    CWH *BC_ptr[2];
+    tut[1]->display();
 
     return 0;
 }
+
+/*
+Rules for virtual functions:
+1- They cannot be static
+2- They are accessed by object pointer
+3- Virtual function can be a friend of another class
+4- A fucntion in base class can might not be used like **virtual void display() in current class**
+5- If a virtual function is defined in a base class, there is no necessity of redefining it in the derived class
+6- If one of the derived class do not have the display function then in place of derived class, base class display
+   function will be called (I M SPECIFICALLY TALKING ABOUT THIS CODE)
+*/
